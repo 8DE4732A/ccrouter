2035a90a42112257d4c65b3647a3b3338fe365bb
liuping <10718172+8DE4732A@users.noreply.github.com>
2026-09-05 08:03:38 +0800

    chore: bump version to 0.7.0

--- internal/server/admin.go
+++ internal/server/admin.go
@@ -17,7 +17,7 @@ import (
 
 func goRuntime() string { return runtime.Version() }
 
-var version = "0.6.1"
+var version = "0.7.0"
 
 func stateOf(c *gin.Context) *gateway.State {
 	v, _ := c.Get("state")
